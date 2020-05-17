3468d248b15af53eb80c85d1fe965a9a102acf57
beshogun <ben.roy@omnitude.tech>
2020-05-17 21:18:33 +0100

    Updates

---
+++ precache-manifest.5b8ad3f3e71b7a9e3e7bc813d6eb52bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7340e91f1c547cb88ed11eb8fd29501c",
+    "url": "/ben-test-react/index.html"
+  },
+  {
+    "revision": "737f87381384f8b08024",
+    "url": "/ben-test-react/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "ce679d7cd789f272adfe",
+    "url": "/ben-test-react/static/css/main.56f66438.chunk.css"
+  },
+  {
+    "revision": "737f87381384f8b08024",
+    "url": "/ben-test-react/static/js/2.b58c47a1.chunk.js"
+  },
+  {
+    "revision": "1aeed1ad10859e42189660fffb44b034",
+    "url": "/ben-test-react/static/js/2.b58c47a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce679d7cd789f272adfe",
+    "url": "/ben-test-react/static/js/main.590ae5f2.chunk.js"
+  },
+  {
+    "revision": "7f45885e84785ca77e07",
+    "url": "/ben-test-react/static/js/runtime-main.9c7b067a.js"
+  }
+]);
